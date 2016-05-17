a3c88cd1909f0a2f846021fe297ae37e4a57ff9e
Yoshie <yoshie.muranaka@mondo.com>
2016-05-17 13:33:47 -0400

    removing/adding plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var csscomb = require('gulp-csscomb');
 var panini = require('panini');
+var uglify = require('gulp-uglify');
+var del = require('del');
 var browserSync = require('browser-sync').create();
 
 var paths = {
-  scripts: 'src/resources/js/**',
+  scripts: 'src/resources/js/**/*',
   images: 'src/resources/images/**',
   scss: 'src/resources/scss/**',
-  html: './src/{layouts,partials,pages,helpers,data}/**/*'
+  html: 'src/{layouts,partials,pages,helpers,data}/**/*'
 };
 
+gulp.task('clean:all', function () {
+  return del([
+    'dist/**/*',
+  ]);
+});
+
+gulp.task('clean:scripts', function () {
+  return del([
+    'dist/scripts/**/*',
+  ]);
+});
+
+gulp.task('clean:images', function () {
+  return del([
+    'dist/images/**/*',
+  ]);
+});
+
+
+/*
+Task for compiling Sass files
+*/
 gulp.task('sass', function () {
   return sass(paths.scss, {
   	sourcemap: false, 
@@ -18,10 +41,22 @@ gulp.task('sass', function () {
   	style: 'compressed'
   })
   .on('error', sass.logError)
-  .pipe(gulp.dest('dist/css'))
+  .pipe(gulp.dest('dist/stylesheets'))
   .pipe(browserSync.stream());
 });
 
+/*
+Task to minify JavaScript files
+*/
+gulp.task('uglify', function() {
+  return gulp.src(paths.scripts)
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+/*
+Task to compile HTML
+*/
 gulp.task('panini', function() {
   gulp.src('src/pages/**/*.html')
     .pipe(panini({
@@ -43,9 +78,12 @@ gulp.task('serve', function() {
 
 gulp.task('watch', function() {
   gulp.watch(paths.scss, ['sass']);
+  gulp.watch(paths.scripts, ['uglify']);
   gulp.watch(paths.html, ['panini']);
 });
 
 
-gulp.task('default', ['sass', 'panini', 'serve', 'watch']);
-gulp.task('watchonly', ['sass', 'panini', 'watch']);
+gulp.task('default', ['sass', 'uglify', 'panini', 'serve', 'watch']);
+gulp.task('watchonly', ['sass', 'uglify', 'panini', 'watch']);
+
+
