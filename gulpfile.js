b25150433daff049d67a448b03716ad31874b9ab
Yoshie <yoshie.muranaka@mondo.com>
2016-05-17 15:32:40 -0400

    adding img move and svgmin tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,35 @@ var sass = require('gulp-ruby-sass');
 var panini = require('panini');
 var uglify = require('gulp-uglify');
 var del = require('del');
+var newer = require('gulp-newer');
+var svgmin = require('gulp-svgmin');
 var browserSync = require('browser-sync').create();
 
 var paths = {
   scripts: 'src/resources/js/**/*',
-  images: 'src/resources/images/**',
-  scss: 'src/resources/scss/**',
+  images: 'src/resources/images/**/*',
+  scss: 'src/resources/scss/*',
   html: 'src/{layouts,partials,pages,helpers,data}/**/*'
 };
 
+/*
+Tasks for cleaning dist folder
+*/
 gulp.task('clean:all', function () {
   return del([
     'dist/**/*',
   ]);
 });
-
 gulp.task('clean:scripts', function () {
   return del([
     'dist/scripts/**/*',
   ]);
 });
-
 gulp.task('clean:images', function () {
   return del([
     'dist/images/**/*',
   ]);
 });
-
 
 /*
 Task for compiling Sass files
@@ -50,6 +52,7 @@ Task to minify JavaScript files
 */
 gulp.task('uglify', function() {
   return gulp.src(paths.scripts)
+    .pipe(newer('dist/scripts'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/scripts'));
 });
@@ -70,6 +73,20 @@ gulp.task('panini', function() {
     .on('finish', browserSync.reload);
 });
 
+/*
+Task to optimize and copy images
+*/
+gulp.task('svgmin', function(){
+  gulp.src('src/resources/images/svg/*.svg')
+  .pipe(svgmin())
+  .pipe(gulp.dest('dist/images/svg'));
+})
+gulp.task('moveImg', ['svgmin'], function() {
+   gulp.src('src/resources/images/**/*.{jpg,png}')
+   .pipe(newer('dist/images'))
+   .pipe(gulp.dest('dist/images'));
+});
+
 gulp.task('serve', function() {
     browserSync.init({
         proxy: "starter.dev"
@@ -80,10 +97,8 @@ gulp.task('watch', function() {
   gulp.watch(paths.scss, ['sass']);
   gulp.watch(paths.scripts, ['uglify']);
   gulp.watch(paths.html, ['panini']);
+  gulp.watch(paths.images, ['moveImg']);
 });
 
-
-gulp.task('default', ['sass', 'uglify', 'panini', 'serve', 'watch']);
-gulp.task('watchonly', ['sass', 'uglify', 'panini', 'watch']);
-
+gulp.task('default', ['sass', 'uglify', 'panini', 'moveImg', 'serve', 'watch']);
 
